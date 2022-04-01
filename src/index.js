92dc5c12a079b143a65cb8f9733a3a561de70629
Elton Bautista <eltonbestaha@gmail.com>
2022-04-01 08:58:18 -0600

    Trying to fetch the weatherAPI but it doesn't seem to be working. I tried using an example from the website but that's also not working. Going to keep working on debugging it.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import './style.css';
 // import { fetchLocation } from './modules/fetch-location';
 // fetchLocation('London');
 import { fetchWeatherConditions } from './modules/fetch-weather-data';
-fetchWeatherConditions();
-// https://openweathermap.org/ Weather API
+fetchWeatherConditions('Calgary');
--- src/modules/fetch-weather-data.js
+++ src/modules/fetch-weather-data.js
@@ -2,12 +2,31 @@ import { fetchLocation } from './fetch-location';
 export const fetchWeatherConditions = async function fetchWeatherConditions(
   city
 ) {
-  //   const dataOfGeocodingAPI = await fetchLocation(city);
-  const dataOfGeocodingAPI = await fetchLocation('Calgary');
+  const dataOfGeocodingAPI = await fetchLocation(city);
+  // commented out for now so I can use 'Calgary' as my test city for the weather API. Will need to use parameters for search form later on.
+  //   const dataOfGeocodingAPI = await fetchLocation('Calgary');
   const longitudeOfCity = dataOfGeocodingAPI.lon;
   const latitudeOfCity = dataOfGeocodingAPI.lat;
   const nameOfCity = dataOfGeocodingAPI.name;
   const stateOfCity = dataOfGeocodingAPI.state;
+  console.log(longitudeOfCity);
 
-  const responseWeatherAPI = await fetch(``);
+  const responseWeatherAPI = await fetch(
+    `api.openweathermap.org/data/2.5/forecast/daily?lat=${latitudeOfCity}&lon=${longitudeOfCity}&cnt=7&appid=d0fb988627c0d201253cf73bc57e0cef`,
+    { mode: 'cors' }
+  );
+  const dataWeatherAPI = await responseWeatherAPI.json();
+  console.log(dataWeatherAPI);
 };
+// POTENTIAL PROMISE VERSION:
+//d0fb988627c0d201253cf73bc57e0cef
+//   fetch(
+//     `api.openweathermap.org/data/2.5/forecast/daily?lat=${latitudeOfCity}&lon=${longitudeOfCity}&cnt=7&appid=d0fb988627c0d201253cf73bc57e0cef`,
+//     { mode: 'cors' }
+//   )
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((response) => {
+//       console.log(response);
+//     });
