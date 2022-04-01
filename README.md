adf52f65d7867b1ae837136b3970c809b7d1452a
Elton Bautista <eltonbestaha@gmail.com>
2022-04-01 09:20:51 -0600

    It looks like I was trying to use a paid version of the API call, I found a free version and it seems to be working perfectly.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # odin-weather-app
-A Weather App that utilizes the OpenWeatherMap API. 
+
+A Weather App that utilizes the OpenWeatherMap API.
+APIs used:
+
+1. Geocoding: https://openweathermap.org/api/geocoding-api
+2. One-Call-API (OpenWeatherMap Free API): https://openweathermap.org/api/one-call-api
--- src/modules/fetch-weather-data.js
+++ src/modules/fetch-weather-data.js
@@ -2,31 +2,35 @@ import { fetchLocation } from './fetch-location';
 export const fetchWeatherConditions = async function fetchWeatherConditions(
   city
 ) {
-  const dataOfGeocodingAPI = await fetchLocation(city);
-  // commented out for now so I can use 'Calgary' as my test city for the weather API. Will need to use parameters for search form later on.
-  //   const dataOfGeocodingAPI = await fetchLocation('Calgary');
-  const longitudeOfCity = dataOfGeocodingAPI.lon;
-  const latitudeOfCity = dataOfGeocodingAPI.lat;
-  const nameOfCity = dataOfGeocodingAPI.name;
-  const stateOfCity = dataOfGeocodingAPI.state;
-  console.log(longitudeOfCity);
+  try {
+    const dataOfGeocodingAPI = await fetchLocation(city);
+    // commented out for now so I can use 'Calgary' as my test city for the weather API. Will need to use parameters for search form later on.
+    //   const dataOfGeocodingAPI = await fetchLocation('Calgary');
+    const longitudeOfCity = dataOfGeocodingAPI.lon;
+    const latitudeOfCity = dataOfGeocodingAPI.lat;
+    const nameOfCity = dataOfGeocodingAPI.name;
+    const stateOfCity = dataOfGeocodingAPI.state;
 
-  const responseWeatherAPI = await fetch(
-    `api.openweathermap.org/data/2.5/forecast/daily?lat=${latitudeOfCity}&lon=${longitudeOfCity}&cnt=7&appid=d0fb988627c0d201253cf73bc57e0cef`,
-    { mode: 'cors' }
-  );
-  const dataWeatherAPI = await responseWeatherAPI.json();
-  console.log(dataWeatherAPI);
+    // POTENTIAL PROMISE VERSION:
+    //d0fb988627c0d201253cf73bc57e0cef
+    //   fetch(
+    //     `https://api.openweathermap.org/data/2.5/onecall?lat=${latitudeOfCity}&lon=${longitudeOfCity}&exclude={part}&appid=d0fb988627c0d201253cf73bc57e0cef`,
+    //     { mode: 'cors' }
+    //   );
+    //     .then((response) => {
+    //       return response.json();
+    //     })
+    //     .then((response) => {
+    //       console.log(response);
+    //     });
+
+    const responseWeatherAPI = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitudeOfCity}&lon=${longitudeOfCity}&exclude={part}&appid=d0fb988627c0d201253cf73bc57e0cef`,
+      { mode: 'cors' }
+    );
+    const dataWeatherAPI = await responseWeatherAPI.json();
+    console.log(dataWeatherAPI);
+  } catch (err) {
+    console.log(err);
+  }
 };
-// POTENTIAL PROMISE VERSION:
-//d0fb988627c0d201253cf73bc57e0cef
-//   fetch(
-//     `api.openweathermap.org/data/2.5/forecast/daily?lat=${latitudeOfCity}&lon=${longitudeOfCity}&cnt=7&appid=d0fb988627c0d201253cf73bc57e0cef`,
-//     { mode: 'cors' }
-//   )
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((response) => {
-//       console.log(response);
-//     });
